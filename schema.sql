26259b678a9425b91c0ca7994c5841cfdadc2dca
Kirk Kornegay <kkornegay1@gmail.com>
2020-12-05 10:56:29 -0500

    changed schema

--- schema.sql
+++ schema.sql
@@ -1,18 +1,21 @@
 --Create Tables
 CREATE TABLE mass_shootings_all(
+	index VARCHAR Primary Key,
 	Incident_Date VARCHAR,
-	State VARCHAR Primary Key,
+	State VARCHAR,
 	Killed INT,
 	Injured INT);
 	
 CREATE TABLE accidental_deaths(
+	index VARCHAR Primary Key,
 	Incident_Date VARCHAR,
-	State VARCHAR Primary Key,
+	State VARCHAR,
 	Killed INT,
 	Injured INT);	
 
 CREATE TABLE officer_involved_shootings(
+	index VARCHAR Primary Key,
 	Incident_Date VARCHAR,
-	State VARCHAR Primary Key,
+	State VARCHAR,
 	Killed INT,
 	Injured INT);	
